005c471d0d67d6687e4c7a74f1ee9952a04cb534
William Clausen <william.h.f.clausen@gmail.com>
2020-04-05 09:43:33 -0700

    add Store library to support requesting data using repo pattern, adds test data/list

--- app/src/main/java/com/cronus/catalogue/MainActivity.kt
+++ app/src/main/java/com/cronus/catalogue/MainActivity.kt
@@ -2,11 +2,31 @@ package com.cronus.catalogue
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.cronus.catalogue.home.ListRepo
+import com.dropbox.android.external.store4.StoreBuilder
+import com.dropbox.android.external.store4.StoreRequest
+import com.dropbox.android.external.store4.get
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
+    private val listRepo = ListRepo()
+    private val listStore = StoreBuilder.fromNonFlow<Unit, List<String>> { _ ->
+        listRepo.getLists()
+    }
+        .disableCache()
+        .build()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        CoroutineScope(Dispatchers.Main).launch {
+            val data = listStore.get(Unit)
+            list.adapter = ArrayAdapter<String>(this@MainActivity, android.R.layout.simple_list_item_1, data)
+        }
     }
 }
---
+++ app/src/main/java/com/cronus/catalogue/home/ListRepo.kt
@@ -0,0 +1,14 @@
+package com.cronus.catalogue.home
+
+class ListRepo {
+
+    fun getLists(): List<String> {
+        return listOf(
+            "test",
+            "data",
+            "rules",
+            "everything",
+            "around",
+            "me")
+    }
+}
